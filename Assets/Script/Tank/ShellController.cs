f037deda74006a17e7d4ceb1591865df73288efc
Park JunHyoung <matte1428@naver.com>
2024-02-02 09:56:10 +0900

    feat:Shell

--- Assets/Script/Tank/ShellController.cs
+++ Assets/Script/Tank/ShellController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class ShellController : MonoBehaviour
 {
+    float shellSpeed = 50.0f;
+    public ParticleSystem explosion;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //GetComponent<Rigidbody>().AddForce(transform.forward * shellSpeed);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("OnTrigger");
+        explosion.transform.position = transform.position;
+        explosion.transform.parent = null;
+        explosion.Play();
+        Destroy(gameObject);
     }
 }
