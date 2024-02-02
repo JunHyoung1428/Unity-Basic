a14d0069dcf33cd191ddf9f987248993f47a242f
Park JunHyoung <matte1428@naver.com>
2024-02-02 16:59:32 +0900

    update

--- Assets/Script/Tank/ShellController.cs
+++ Assets/Script/Tank/ShellController.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class ShellController : MonoBehaviour
 {
-    float shellSpeed = 50.0f;
     public ParticleSystem explosion;
 
     // Start is called before the first frame update
@@ -21,7 +20,6 @@ public class ShellController : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("OnTrigger");
         explosion.transform.position = transform.position;
         explosion.transform.parent = null;
         explosion.Play();
